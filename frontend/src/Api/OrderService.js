9f5b5f436a321076c66ea92a31002dea3cf8f279
hak.dok0@gmail.com <hak.dok0@gmail.com>
2023-06-10 00:38:32 +0300

    fix sert add config file .env.development.local

--- frontend/src/Api/OrderService.js
+++ frontend/src/Api/OrderService.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 export default class OrderService {
+    static ipServer = process.env.REACT_APP_IP_SERVER; 
 
     static async getAll() {
-        const response = await axios.get("http://localhost:5140/api/Order/all");
+        console.log(this.ipServer)
+        const response = await axios.get(`${this.ipServer}/api/Order/all`);
         return response;
     }
 
     static async createOrder(order) {
-        const response = await axios.post("http://localhost:5140/api/Order/createOrder", order);
+        const response = await axios.post(`${this.ipServer}/api/Order/createOrder`, order);
         return response;
     }
     static async getbyIdOrder(id) {
-        const response = await axios.get(`http://localhost:5140/api/Order/${id}`);
+        const response = await axios.get(`${this.ipServer}/api/Order/${id}`);
         return response;
     }
 }
